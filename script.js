5b0061f804379afd3b7290d6a64ac78cea573f5d
Hendrik <schmurgler7@gmail.com>
2025-08-23 21:56:52 +0200

    asdf

---
+++ script.js
@@ -0,0 +1,16 @@
+const elements = document.querySelectorAll('.fade-in, .fade-in-right');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // reinfaden
+      entry.target.classList.add('visible');
+    } else {
+      // rausfaden
+      entry.target.classList.remove('visible');
+    }
+  });
+}, { threshold: 0.3}); 
+// threshold = wieviel % sichtbar sein müssen (hier 10%)
+
+elements.forEach(el => observer.observe(el));
